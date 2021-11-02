5c02269956953e36e0632d7c49a73059db29f7b7
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-02 23:38:38 +0530

    Refractor checking equality of 2 lines using java equals method

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -21,9 +21,35 @@ public class LineComparision {
 		System.out.println("Enter y2 : ");
 		double y2 = sc.nextDouble();
 		
-		double length =Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+		double length1 =Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+		System.out.println(length1);
 		
-		System.out.println(length);
+		System.out.println("Enter x3 : ");
+		double x3 = sc.nextDouble();
+		
+		System.out.println("Enter y3 : ");
+		double y3 = sc.nextDouble();
+		
+		System.out.println("Enter x4 : ");
+		double x4 = sc.nextDouble();
+		
+		System.out.println("Enter y4 : ");
+		double y4 = sc.nextDouble();
+		
+		double length2 = Math.sqrt(Math.pow(x4-x3, 2)+Math.pow(y4-y3, 2));
+		System.out.println(length2);
+		
+		Double le1=length1;
+		Double le2=length2;
+		
+		if(le1.equals(le2)) {
+			System.out.println("equal");
+		}
+		else {
+			System.out.println("not equal");
+		}
+		
+		
 	}
 
 }
